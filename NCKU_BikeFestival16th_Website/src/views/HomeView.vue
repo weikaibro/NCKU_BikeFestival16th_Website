7af4fd6752407f9d09141c50049b73ac0e544cee
weikaichen <wayne900719@gmail.com>
2023-01-14 16:41:02 +0800

    include TailwindCSS and simplify vite-vue template

--- NCKU_BikeFestival16th_Website/src/views/HomeView.vue
+++ NCKU_BikeFestival16th_Website/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Hello world!
+  </div>
 </template>
