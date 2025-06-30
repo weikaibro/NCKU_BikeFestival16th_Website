1dfe2320ec9d21ff694bcdf02b90dca1de871214
unknown <wayne900719@gmail.com>
2025-06-30 17:43:11 +0800

    delete the file

--- NCKU_BikeFestival16th_Website/src/stores/memberInfo.js
+++
@@ -1,9 +0,0 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useMemberStore = defineStore('memberInfo', () => {
-
-  
-
-  return { scrollLeft, scrollRight }
-})
